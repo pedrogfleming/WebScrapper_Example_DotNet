5e2d23379aa6b9614a5845ec3365c34fbba867d9
Pedro Fleming <pedro.fleming@globallogic.com>
2022-09-02 13:25:19 -0300

    Example done

---
+++ WebScapper_Test/WebScapper_Test/Controllers/HomeController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using HtmlAgilityPack;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Net;
+using System.Text;
+using System.IO;
+using WebScapper_Test.Models;
+using System.Linq.Expressions;
+
+namespace WebScapper_Test.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            string url = "https://en.wikipedia.org/wiki/List_of_programmers";
+            var response = CallUrl(url).Result;
+            var wikiLinks = ParseHtml(response);
+            WriteToCsv(wikiLinks);
+            return View();
+        }
+        private static async Task<string> CallUrl(string fullUrl)
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetStringAsync(fullUrl);
+            return response;
+        }
+        private List<string> ParseHtml(string html)
+        {
+            HtmlDocument htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(html);
+
+            var programmerLinks = htmlDoc.DocumentNode.Descendants("li")
+                .Where(node => !node.GetAttributeValue("class", "").Contains("tocsection"))
+                .ToList();
+
+            List<string> wikiLink = new List<string>();
+
+            foreach (var link in programmerLinks)
+            {
+                if (link.FirstChild.Attributes.Count > 0) wikiLink.Add("https://en.wikipedia.org/" + link.FirstChild.Attributes[0].Value);
+            }
+            #region it will be much more convenient to apply an XPath expression
+            //One thing to note, we wouldn't have needed to perform the element selection manually.
+            //We did so solely for the sake of the example.
+            //In real-world code, it will be much more convenient to apply an XPath expression.
+            //With that, our whole selection logic would fit into a one-liner.
+            //var programmerLinks = htmlDoc.DocumentNode.SelectNodes("//li[not(contains(@class, 'tocsection'))]")
+            #endregion
+            return wikiLink;
+        }
+        private void WriteToCsv(List<string> links)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var link in links)
+            {
+                sb.AppendLine(link);
+            }
+
+            System.IO.File.WriteAllText("links.csv", sb.ToString());
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebScapper_Test/WebScapper_Test/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebScapper_Test.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
